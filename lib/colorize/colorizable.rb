36935f70aba6c65032a828a8bbd212bbb2d664e8
Clay Dunston <claydunston@gmail.com>
2022-03-19 11:11:07 -0500

    [WIP] document meta methods

---
+++ lib/colorize/colorizable.rb
@@ -0,0 +1,45 @@
+# class Colorizable
+#     # default
+#     # red
+#     # black
+#     # green
+#     # yellow
+#     # blue
+#     # magenta
+#     # cyan
+#     # white
+#     # light_black
+#     # light_red
+#     # light_green
+#     # light_yellow
+#     # light_blue
+#     # light_magenta
+#     # light_cyan
+#     # light_white
+#
+#     # on_default
+#     # on_red
+#     # on_black
+#     # on_green
+#     # on_yellow
+#     # on_blue
+#     # on_magenta
+#     # on_cyan
+#     # on_white
+#     # on_light_black
+#     # on_light_red
+#     # on_light_green
+#     # on_light_yellow
+#     # on_light_blue
+#     # on_light_magenta
+#     # on_light_cyan
+#     # on_light_white
+#
+#     # default   - Turn off all attributes
+#     # bold      - Set bold mode
+#     # italic    - Set italic mode
+#     # underline - Set underline mode
+#     # blink     - Set blink mode
+#     # swap      - Exchange foreground and background colors
+#     # hide      - Hide text (foreground color would be the same as background)
+# end
